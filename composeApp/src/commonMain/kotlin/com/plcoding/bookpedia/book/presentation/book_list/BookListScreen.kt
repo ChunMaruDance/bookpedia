f34941086a1d34712282b3db84a0ec525fce3a64
MVY <MVY@d.c>
2025-01-04 20:31:21 +0200

    add book screen structure

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListScreen.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListScreen.kt
@@ -1,7 +1,39 @@
 package com.plcoding.bookpedia.book.presentation.book_list
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.plcoding.bookpedia.book.domain.Book
+import org.koin.compose.viewmodel.koinViewModel
+
 
 @Composable
-fun BookListScreen() {
+fun BookListScreenRoot(
+    viewModel: BookListViewModel = koinViewModel(),
+    onBookClick: (Book) -> Unit
+) {
+
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    BookListScreen(
+        state = state,
+        onAction = { action ->
+            when (action) {
+                is BookListAction.OnBookClick -> onBookClick(action.book)
+                else -> Unit
+
+            }
+            viewModel.onAction(action)
+
+        }
+    )
+
+}
+
+@Composable
+private fun BookListScreen(
+    state: BookListState,
+    onAction: (BookListAction) -> Unit
+) {
+
 }
