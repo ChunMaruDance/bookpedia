46462b3a7feb878e236146d59e857bc3c68a736f
MVY <MVY@d.c>
2025-01-04 21:25:33 +0200

    add searh bar component to screen

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
@@ -3,10 +3,18 @@ package com.plcoding.bookpedia
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
+import com.plcoding.bookpedia.book.presentation.book_list.BookListScreenRoot
+import com.plcoding.bookpedia.book.presentation.book_list.BookListViewModel
 
 @Composable
 fun App() {
     MaterialTheme {
-       //todo navigation
+        //todo navigation
+
+        BookListScreenRoot(
+            onBookClick = {},
+            viewModel = remember { BookListViewModel() }
+        )
+
     }
 }
