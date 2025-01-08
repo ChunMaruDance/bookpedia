116b6e92d8894ae2b488ec50a532ae2de1701121
MVY <MVY@d.c>
2025-01-08 12:45:29 +0200

    add titled content

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/core/presentation/components/TitledContent.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/core/presentation/components/TitledContent.kt
@@ -1,7 +1,29 @@
 package com.plcoding.bookpedia.core.presentation.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
-fun TitledContent() {
+fun TitledContent(
+    title: String,
+    modifier: Modifier,
+    content: @Composable () -> Unit
+) {
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            title,
+            style = MaterialTheme.typography.titleSmall
+        )
+        content()
+
+    }
+
 }
